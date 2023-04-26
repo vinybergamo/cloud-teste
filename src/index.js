1bd067ec0071f4bdb75d5c097123faa81811c947
vinybergamo <vinybergamo@gmail.com>
2023-04-25 23:13:53 -0300

    feat: webserver configured

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const expree = require("express");
 
 const app = expree();
 
-const router = expree.Router();
-
 app.get("/", (req, res) => {
   return res.status(200).json({
     message: "Hello World",
